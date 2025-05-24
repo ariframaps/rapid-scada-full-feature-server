325335e3105192a1b22c81287b46e98dc5edc601
ariframaps <ariframa@student.ub.ac.id>
2025-05-24 17:57:20 +0700

    fix port

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/scada", scadaRoutes);
 app.use("/api/schedule", scheduleRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // const cron = require('node-cron');
